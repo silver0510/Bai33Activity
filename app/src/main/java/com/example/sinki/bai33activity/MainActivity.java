e3158f685f6a7b3cf2ec96959419d37a2c30fca4
Nghia Pham <phunghia91bkdn@gmail.com>
2018-02-03 12:04:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sinki/bai33activity/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.sinki.bai33activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void xuLyCheKhuatToanBo(View view) {
+        Intent intent = new Intent(MainActivity.this,ManHinh2Activity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(MainActivity.this,"onStart",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(MainActivity.this,"onStop",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(MainActivity.this,"onDestroy",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(MainActivity.this,"onPause",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(MainActivity.this,"onResume",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(MainActivity.this,"onRestart",Toast.LENGTH_LONG).show();
+    }
+
+    public void xuLyCheKhuat1Phan(View view) {
+        Intent intent = new Intent(MainActivity.this,ManHinh3Activity.class);
+        startActivity(intent);
+    }
+}
